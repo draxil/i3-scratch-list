9222a455e6d96fb63dbc98bb6567e63639bf41c0
Joe Higton <draxil@gmail.com>
2021-02-04 11:30:51 +0000

    first functiong version

---
+++ i3-scratch-list.go
@@ -0,0 +1,33 @@
+package main
+
+import ("go.i3wm.org/i3/v4"
+	"fmt"
+	"flag"
+)
+
+func main(){
+	noid := flag.Bool("noid", false, "Don't give me the window id")
+	flag.Usage = func(){
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage: i3-scratch-list [-noid]\n\n")
+		flag.PrintDefaults()
+		
+	}
+	flag.Parse()
+	
+	ws, _ := i3.GetTree()
+	sp := ws.Root.FindChild(
+		func(n *i3.Node) bool {
+			return n.Name == "__i3_scratch"
+		},
+	)
+
+	for _, n := range sp.FloatingNodes {
+		for _, wn := range n.Nodes {
+			if ! *noid {
+				fmt.Println(wn.Window, " - ", wn.Name)
+			} else {
+				fmt.Println(wn.Name)
+			}
+		}
+	}
+}
